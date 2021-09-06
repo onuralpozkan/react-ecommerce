c875499daa049abfb7f8f83d88b522006af1265a
onuralpozkan <onuralpozkan01@gmail.com>
2021-09-06 11:39:11 +0300

    cart info in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,31 @@
-import styles from "./Header.module.css";
 import { useState } from "react";
+import "./Header.css";
 import Nav from "../Navigation/Nav";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const bars = <i class="las la-bars"></i>;
   const times = <i class="las la-times"></i>;
+
+  const state = useSelector((state) => state.cartReducer);
+
   return (
-    <header className={styles.header}>
-      <span onClick={() => setIsMenuOpen(!isMenuOpen)} className={styles.menu}>
+    <header className="header">
+      <span onClick={() => setIsMenuOpen(!isMenuOpen)} className="menu">
         {isMenuOpen ? times : bars} <span>Kategoriler</span>
       </span>
-      <span className={styles.brand}>
+      <span className="brand">
         <Link to="/">Brand</Link>
       </span>
-      <span className={styles.login}>
+      <span className="login">
         <i className="las la-user"></i>
       </span>
-      <span className={styles.cart}>
+      <span className="cart">
         <Link to="/cart">
           <i class="las la-shopping-cart"></i>
         </Link>
+        <span className="cartInfo">{state.length}</span>
       </span>
 
       <Nav clsName={isMenuOpen && "open"} />
